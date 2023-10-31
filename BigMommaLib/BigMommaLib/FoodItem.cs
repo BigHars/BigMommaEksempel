6f355a0b6c8f5d39133137540224bdae69bc3e42
hasse <hass2699@edu.zealand.dk>
2023-10-31 12:25:49 +0100

    added classes

---
+++ BigMommaLib/BigMommaLib/FoodItem.cs
@@ -0,0 +1,39 @@
+﻿namespace BigMommaLib
+{
+    public class FoodItem
+    {
+        //instance field
+        private string _name;
+        private string _description;
+        private double _price;
+
+        //properties
+        public string Name 
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public FoodItem(string name, string description, double price)
+        {
+            _name = name;
+            _description = description;
+            _price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Description: {Description}, Price: {Price}";
+        }
+    }
+}
